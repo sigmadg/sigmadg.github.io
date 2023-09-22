4751fa76f5b2f071ae7a0282d989a81ed6300395
sigmadg <sigma.dexter.gaby@gmail.com>
2023-09-21 22:35:55 -0600

    Actualización

--- Bingo-JS-master/bolas.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $bombo = $_REQUEST["numeros"];
-  $nbolas = count($bombo);
-  $bola = rand(0,$nbolas-1);
-  echo $bola;
-?>
--- Cuadro_Magico/code/bucketstore/store.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-
-    //alert('started the store');
-    jQuery('.number-of-buckets').html( numBuckets );
-
-});
